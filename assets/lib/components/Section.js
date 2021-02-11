5093366cdcedfbd4561980ef909f5b180b4f392f
Chris Perko <chris@chrisperko.net>
2021-02-11 18:01:50 -0500

    Initial styling

--- assets/lib/components/Section.js
+++ assets/lib/components/Section.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ControlList from "./ControlList";
 
 export default class Section extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.click = this.click.bind(this);
+    }
+
+    state = {
+        open: false
+    }
+
+    click() {
+        this.setState({open: !this.state.open});
+    }
+
+    renderControls() {
+        if (this.state.open) {
+            return (<ControlList controls={this.props.data.controls}/>);
+        }
+    }
+
     render() {
         return (
-            <div className="wpcui_section">
-                <h4>{this.props.data.title}</h4>
-                <ControlList controls={this.props.data.controls} />
-            </div>
+            <li onClick={this.click} className="accordion-section control-panel-themes wpcui-section">
+                <h3 className="accordion-section-title">{this.props.data.title}</h3>
+                {this.renderControls()}
+            </li>
         )
     }
 }
--- assets/lib/components/SectionList.js
+++ assets/lib/components/SectionList.js
@@ -2,11 +2,22 @@ import React from "react";
 import Section from "./Section";
 
 export default class SectionList extends React.Component {
-    render() {
+
+    getSections() {
         console.log(Object.values(this.props.sections));
 
         const sections = Object.values(this.props.sections);
 
-        return sections.map(section => <Section key={section.id} data={section} />);
+        return sections.map(section => <Section key={section.id} data={section}/>);
+    }
+
+    render() {
+        return (
+            <div className="customize-sidebar-outer-content wpcui-section-list">
+                <div className="customize-pane-parent">
+                    {this.getSections()}
+                </div>
+            </div>
+        )
     }
 }
