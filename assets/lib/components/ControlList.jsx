8336dd7830a078480f6e46f02d6225fff1a6f761
Chris Perko <chris@chrisperko.net>
2021-02-19 17:35:12 -0500

    Refactored code to use db v2.0 style

--- assets/lib/components/ControlList.jsx
+++ assets/lib/components/ControlList.jsx
@@ -4,10 +4,6 @@ import Control from "./Control";
 
 export default class ControlList extends React.Component {
     render() {
-        console.log(Object.values(this.props.controls));
-
-        const controls = Object.values(this.props.controls);
-
-        return controls.map(control => <Control key={control.id} data={control} />);
+        return this.props.controls.map(control => <Control key={control.id} data={control} />);
     }
 }
--- assets/lib/components/SectionList.jsx
+++ assets/lib/components/SectionList.jsx
@@ -4,11 +4,7 @@ import Section from "./Section";
 export default class SectionList extends React.Component {
 
     getSections() {
-        console.log(Object.values(this.props.sections));
-
-        const sections = Object.values(this.props.sections);
-
-        return sections.map(section => <Section key={section.id} data={section}/>);
+        return this.props.sections.map(section => <Section key={section.id} data={section}/>);
     }
 
     render() {
