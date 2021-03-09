dadfa165935ec634b638c9a5a482f9932a676b42
Chris Perko <chris@chrisperko.net>
2021-03-09 17:49:44 -0500

    Created the EditSectionForm

---
+++ assets/lib/forms/EditSectionForm.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import Button from "../elements/Button";
+import store, { actions } from "../redux/wpcuiReducer";
+import FormTextInput from "../elements/FormTextInput";
+import FormCancel from "../elements/FormCancel";
+import { stringToSnakeCase } from "../common";
+import FormCheckbox from "../elements/FormCheckbox";
+import WarningBar from "../elements/WarningBar";
+
+export default class EditSectionForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sectionId: "",
+      sectionTitle: "",
+      autoGenerateId: "checked",
+      errorTitle: "",
+      errorMessage: "",
+    };
+
+    this.handleSectionTitleChange = this.handleSectionTitleChange.bind(this);
+    this.handleSectionIdChange = this.handleSectionIdChange.bind(this);
+    this.updateSection = this.updateSection.bind(this);
+    this.closeForm = this.closeForm.bind(this);
+    this.handleAutoGenerateIdChange = this.handleAutoGenerateIdChange.bind(
+      this
+    );
+  }
+
+  /**
+   * Update the section with the values provided in the form.
+   */
+  updateSection() {
+    if (!this.state.sectionId || !this.state.sectionTitle) {
+      this.setState({
+        errorTitle: "Missing Required Fields",
+        errorMessage: "Section Title and ID are required.",
+      });
+      return;
+    }
+
+    const updatedSection = {
+      id: this.state.sectionId,
+      title: this.state.sectionTitle,
+      priority: 99,
+      visible: true,
+      controls: [],
+    };
+
+    store.dispatch({ type: actions.CREATE_SECTION, section: updatedSection });
+
+    this.closeForm();
+  }
+
+  closeForm() {
+    this.props.onClose();
+  }
+
+  handleSectionTitleChange(event) {
+    this.setState({ sectionTitle: event.target.value });
+
+    if (this.state.autoGenerateId) {
+      this.setState({ sectionId: stringToSnakeCase(event.target.value) });
+    }
+  }
+
+  handleSectionIdChange(event) {
+    this.setState({ sectionId: event.target.value });
+  }
+
+  handleAutoGenerateIdChange() {
+    if (!this.state.autoGenerateId) {
+      this.setState({
+        newSectionId: stringToSnakeCase(this.state.sectionTitle),
+      });
+    }
+
+    this.setState({ autoGenerateId: !this.state.autoGenerateId });
+  }
+
+  render() {
+    return (
+      <div className="wpcui-modal-wrapper">
+        <div className="wpcui-modal">
+          <h3>Edit Customizer Section</h3>
+          <WarningBar
+            title={this.state.errorTitle}
+            innerText={this.state.errorMessage}
+          />
+          <table className="form-table">
+            <tbody>
+              <FormTextInput
+                label="Section Title"
+                inputId="sectionTitle"
+                placeholder="Section Name"
+                onChange={this.handleSectionTitleChange}
+                value={this.state.sectionTitle}
+              />
+              <FormCheckbox
+                label="Auto-Generate ID"
+                checked={this.state.autoGenerateId}
+                handleChange={this.handleAutoGenerateIdChange}
+              />
+              <FormTextInput
+                label="Section ID"
+                inputId="sectionId"
+                placeholder="Section ID"
+                onChange={this.handleSectionIdChange}
+                value={this.state.sectionId}
+                disabled={this.state.autoGenerateId}
+              />
+            </tbody>
+          </table>
+          <Button
+            buttonType="primary"
+            innerText="Update Section"
+            click={this.updateSection}
+          />
+          <FormCancel handleClick={this.closeForm} />
+        </div>
+      </div>
+    );
+  }
+}
