4bd8fcc6b4140834578fc79ac0d529bf7a49874a
Chris Perko <chris@chrisperko.net>
2021-04-08 17:26:59 -0400

    Database now ts

--- assets/lib/services/database.ts
+++ assets/lib/services/database.ts
@@ -1,6 +1,7 @@
 import { saveData } from "./api";
+import { Control, DatabaseObject, Section } from "../models/models";
 
-export function upgrade(data) {
+export function upgrade(data): void {
   switch (data.db_version) {
     case 1:
       upgradeToVersion2(data);
@@ -15,18 +16,18 @@ export function upgrade(data) {
  * stored as an array.
  * @param data
  */
-function upgradeToVersion2(data) {
-  let newData = { db_version: 2, panels: [], sections: [] };
+function upgradeToVersion2(data): void {
+  let newData: DatabaseObject = { db_version: 2, panels: [], sections: [] };
 
   const sections = Object.values(data.sections);
-  sections.forEach((section) => {
+  sections.forEach((section: Section) => {
     let newSection = section;
     const controls = Object.values(section.controls);
 
     // clear the controls to be re-built
     newSection.controls = [];
 
-    controls.forEach((control) => {
+    controls.forEach((control: Control) => {
       newSection.controls.push(control);
     });
 
